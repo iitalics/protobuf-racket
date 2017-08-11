8dd07fb2262485b1a557403716115949501ce1e5
Milo Turner <iitalics@gmail.com>
2017-08-11 14:34:55 -0400

    getting the parser going

--- protobuf/private/grammar.rkt
+++ protobuf/private/grammar.rkt
@@ -10,14 +10,27 @@
 (define current-parse-source-path
   (make-parameter #f))
 
+
+(define (position->srcloc pos)
+  (make-srcloc (current-parse-source-path)
+               (position-line pos)
+               (position-col pos)
+               (position-offset pos)
+               #f))
+
 (define (raise-parse-error tok-ok? tok-name tok-value
                            start-pos end-pos)
-  (let ([src (make-srcloc (current-parse-source-path)
-                          #f #f
-                          start-pos
-                          (- end-pos start-pos))]
-        [msg (format "invalid token: ~a"
-                     (protobuf-token->string tok-name))])
+  (let* ([off1 (position-offset start-pos)]
+         [off2 (position-offset end-pos)]
+         [line (position-line start-pos)]
+         [col (position-col start-pos)]
+         [src (make-srcloc (current-parse-source-path)
+                           line
+                           col
+                           off1
+                           (- off2 off1))]
+         [msg (format "invalid token: ~a"
+                      (protobuf-token->string tok-name))])
     (raise (exn:fail:read msg
                           (current-continuation-marks)
                           (list src)))))
@@ -44,6 +57,7 @@
              (parse/gen gen)))
 
          (define (parse/port port)
+           (port-count-lines! port)
            (parse/seq (in-protobuf-tokens port #:include-eof? #t)))
 
          (define (parse x)
@@ -57,9 +71,41 @@
              [(procedure? x) (parse/gen x)]
              [else (error "invalid parse input:" x)])))]))
 
+(define-syntax $1-src
+  (syntax-parser
+    [(_)
+     #:with $1-start-pos (datum->syntax this-syntax '$1-start-pos)
+     #'(position->srcloc $1-start-pos)]))
+
+
+;; using as reference:
+;;   https://developers.google.com/protocol-buffers/docs/reference/proto3-spec
+
+;; TODO: recursive decent, or maybe look into parser combinator libraries.
+;;       that would produce better error messages, and working with parser-tools/yacc
+;;       kinda sucks anyways.
 
 (define-parser parse-ast
   #:start <file>
   #:end EOF
-  (<file> [(KW-syntax EQ IDENT SEMI)
-           (format "the protocol version is: ~a" $3)]))
+  (<file>
+   [(<syntax> <toplevels>)
+    (begin (printf "the protocol version is: ~a\n" $1)
+           $2)])
+
+  (<syntax>
+   [(KW-syntax EQ STRINGLIT SEMI) $3])
+
+  (<toplevels>
+   [(<toplevels> <import>) (cons $2 $1)]
+   [(<toplevels> <empty>) $1]
+   [() '()])
+
+  (<import>
+   [(KW-import STRINGLIT SEMI) (ast:import ($1-src) $2 #f)]
+   [(KW-import KW-public STRINGLIT SEMI) (ast:import ($1-src) $3 #t)])
+
+  (<empty>
+   [(SEMI) (void)])
+
+  )
