39e0f5f1751f7db0b982edbe379f7da236139882
Milo Turner <iitalics@gmail.com>
2017-08-12 13:54:59 -0400

    tests for parsing options, messages, enums

--- protobuf/tests/parser.rkt
+++ protobuf/tests/parser.rkt
@@ -20,8 +20,9 @@
        #:with [token+eof ...] #'[token ... EOF]
        #:with [token-expr ...]
        (stx-map (syntax-parser
-                  [TOK:id #''TOK]
-                  [(TOK:id val)
+                  [TOK:id #''TOK]            ; TOK      =>  'TOK
+                  [x:str #'(token-IDENT 'x)] ; "ID"     =>  (token-IDENT "ID")
+                  [(TOK:id val)              ; (TOK x)  =>  (token-TOK x)
                    #:with token-TOK (format-id #'TOK "token-~a" #'TOK)
                    #'(token-TOK val)])
                 #'[token+eof ...])
@@ -54,7 +55,7 @@
 
   (parse-test [
                KW-syntax EQ (STRINGLIT "proto3") SEMI
-               KW-package (IDENT "foo") DOT (IDENT "bar") SEMI
+               KW-package "foo" DOT "bar" SEMI
                ]
               (ast:package $5-src "foo.bar"))
 
@@ -66,5 +67,48 @@
               (ast:import $5-src "foo/bar" #f)
               (ast:import $8-src "foo/baz" #t))
 
+  (parse-test [
+               KW-syntax EQ (STRINGLIT "proto3") SEMI
+               KW-option "option1" DOT "x" EQ KW-true SEMI
+               KW-option LP "option2" DOT "y" RP DOT "z" EQ MINUS (INTLIT 3) SEMI
+               ]
+              (ast:option $5-src #f '("option1" "x") #t)
+              (ast:option $12-src "option2.y" '("z") -3))
+
+  (parse-test [
+               KW-syntax EQ (STRINGLIT "proto3") SEMI
+               KW-message "Msg1" LC
+               KW-repeated KW-int32 "x" EQ (INTLIT 3) SEMI
+               DOT "test" DOT "Msg2" "y" EQ (INTLIT 4) SEMI
+               KW-reserved (INTLIT 3)
+               COMMA (INTLIT 4) KW-to (INTLIT 6)
+               COMMA (INTLIT 10) KW-to KW-max SEMI
+               KW-reserved (STRINGLIT "z") COMMA (STRINGLIT "w") SEMI
+               RC
+               ]
+              (ast:message $5-src
+                           "Msg1"
+                           (list (ast:field $8-src "x" 3 'repeated 'int32 '())
+                                 (ast:field $14-src "y" 4 'optional ".test.Msg2" '()))
+                           '() '() '() '()
+                           (list 3
+                                 (ast:range $25-src 4 6)
+                                 (ast:range $29-src 10 'max)
+                                 "z"
+                                 "w")
+                           '()))
+
+  (parse-test [
+               KW-syntax EQ (STRINGLIT "proto3") SEMI
+               KW-enum "Enum1" LC
+               "A" EQ (INTLIT 0) SEMI
+               "B" EQ (INTLIT 6) SEMI
+               RC
+               ]
+              (ast:enum $5-src
+                        "Enum1"
+                        (list (ast:enum-val $8-src "A" 0 '())
+                              (ast:enum-val $12-src "B" 6 '()))
+                        '()))
 
   )
