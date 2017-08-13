07541aa2fd2675fc3d75f10f0828321875f973e9
Milo Turner <iitalics@gmail.com>
2017-08-13 14:19:26 -0400

    compiler creates descriptors and identifies some name conflicts

--- protobuf/private/compiler.rkt
+++ protobuf/private/compiler.rkt
@@ -35,8 +35,29 @@
                   [path (ast-source root)]
                   [package (ast:root-package root)]))
 
-  ;; TODO: populate dependencies
-  ;; TODO: create type descriptors
+  (define deps
+    (for/list ([imp (in-list (ast:root-imports root))])
+      (get-file-desc (unbox (ast:import-resolved-path-box imp)))))
+
+  (send fd set-dependencies deps)
+  (send fd set-public-dependencies
+        (for/list ([imp (in-list (ast:root-imports root))]
+                   [dep (in-list deps)]
+                   #:when (ast:import-public? imp))
+          dep))
+
+  (verify-no-type-conflicts
+   (ast:root-messages root) ast:message-name
+   (ast:root-enums root) ast:enum-name)
+
+  (send fd set-message-types
+        (map ast:message->descriptor%
+             (ast:root-messages root)))
+
+  (send fd set-enum-types
+        (map ast:enum->enum-descriptor%
+             (ast:root-enums root)))
+
   ;; TODO: compile file options
   ;; TODO: compile message fields
   ;; TODO: compile enum values
@@ -45,3 +66,48 @@
              (ast-source root)
              root)
   fd)
+
+
+;; convert ast to descriptor%
+;; does not compile any fields, just nested types.
+(define (ast:message->descriptor% ast)
+  (define msgd (new descriptor%
+                  [name (ast:message-name ast)]))
+
+  (verify-no-type-conflicts
+   (ast:message-nested-msgs ast) ast:message-name
+   (ast:message-nested-enums ast) ast:enum-name)
+
+  (send msgd set-nested-types
+        (map ast:message->descriptor%
+             (ast:message-nested-msgs ast)))
+  (send msgd set-nested-enums
+        (map ast:enum->enum-descriptor%
+             (ast:message-nested-enums ast)))
+  ;; TODO: compile message options
+  msgd)
+
+
+;; convert ast to enum-descriptor%
+;; does not compile values
+(define (ast:enum->enum-descriptor% enum)
+  (new enum-descriptor%
+       [name (ast:enum-name enum)])
+  ;; TODO: compile enum options
+  )
+
+
+;; raises an error if any of the messages or enums have conflicting names
+(define (verify-no-type-conflicts . args)
+  (define names (mutable-set))
+  (let trav ([args args])
+    (match args
+      ['() (void)]
+      [(list* asts get-name rest)
+       (for ([ast (in-list asts)])
+         (when (set-member? names (get-name ast))
+           (raise-compile-error (ast-loc ast)
+                                "encountered multiple types named ~v"
+                                (get-name ast)))
+         (set-add! names (get-name ast)))
+       (trav rest)])))
