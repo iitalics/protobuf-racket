d5c6d4efbc17be0c40def0b75f349f897d567ecf
Milo Turner <iitalics@gmail.com>
2017-08-14 16:21:59 -0400

    rewriting compiler.rkt

--- protobuf/private/compiler.rkt
+++ protobuf/private/compiler.rkt
@@ -3,10 +3,11 @@
          "dependencies.rkt"
          "descriptors.rkt")
 
-(provide current-file-desc-pool
-         current-names-in-use
-         compile-root
-         (struct-out exn:fail:compile))
+(provide with-clean-compile
+         all-descriptors
+         current-scope
+         (struct-out exn:fail:compile)
+         ast->descriptor)
 
 
 (define-struct (exn:fail:compile exn:fail:read) ())
@@ -18,141 +19,58 @@
           (list loc loc))))
 
 
-;; maps resolved paths to generated file descriptors
-;; (hash complete-path? => (is-a?/c file-descriptor%))
-(define current-file-desc-pool
-  (make-parameter (make-hash)))
-
-(define (get-file-desc resolved-path)
-  (hash-ref (current-file-desc-pool)
-            resolved-path))
-
-
-;; maps full names (e.g. "google.protobuf.Any") to the file
-;; they were declared in. includes package paths (e.g. "google" and "google.protobuf")
-;; which map to the file they were first declared in
-;; (hash string? => complete-path?)
-(define current-names-in-use
-  (make-parameter (make-hash)))
-
 (define (name-append prefix post)
   (if (equal? prefix "")
       post
       (string-append prefix "." post)))
 
-;; errors if the name is already in use, otherwise
-;; registers it and returns (void)
-(define (check-in-use name loc)
-  (cond
-    [(hash-ref (current-names-in-use) name #f)
-     => (λ (use-path)
-          (raise-compile-error loc
-                               "name ~v already bound in ~a"
-                               name
-                               (path->string (normalize-path use-path))))]
-
-    [else
-     (hash-set! (current-names-in-use)
-                name
-                (srcloc-source loc))]))
 
 
-;; fully compile root AST into file-descriptor% object
-;; additionally adds it to the file desc pool
-(define (compile-root root)
-  (define fd (new file-descriptor%
-                  [path (ast-source root)]
-                  [package (ast:root-package root)]
-                  [origin-ast root]))
+;; maps resolved paths to generated file descriptors
+;; (hash complete-path? => (is-a?/c file-descriptor%))
+(define file-descriptor-pool (make-parameter (make-hash)))
 
-  ; register package path
-  (let ([pkg-path (send fd get-package-path)])
-    (for ([i (in-range (length pkg-path))])
-      (hash-ref! (current-names-in-use)
-                 (take pkg-path (add1 i))
-                 (ast-source root))))
+;; maps qualified names to descriptors
+;; e.g. [file 'descriptor.proto']
+;;     syntax = 'proto3';
+;;     package google.protobuf;
+;;     message Field {
+;;       enum Type { UINT32 = 0; ... }
+;;       Type type = 1;
+;;     }
+;; would be generated into
+;;   google                        =>  <file-descriptor% 'descriptor.proto'>
+;;   google.protobuf               =>  <file-descriptor% 'descriptor.proto'>
+;;   google.protobuf.Field         =>  <descriptor% 'Field'>
+;;   google.protobuf.Field.Type    =>  <enum-descriptor% 'Type'>
+;;   google.protobuf.Field.UINT32  =>  <enum-value% 'UINT32'>
+;;   google.protobuf.Field.type    =>  <field-descriptor% 'Type'>
+(define all-descriptors (make-parameter (make-hash)))
 
-  (define deps
-    (for/list ([imp (in-list (ast:root-imports root))])
-      (get-file-desc (unbox (ast:import-resolved-path-box imp)))))
-
-  (send fd set-dependencies deps)
-
-  (send fd set-public-dependencies
-        (for/list ([imp (in-list (ast:root-imports root))]
-                   [dep (in-list deps)]
-                   #:when (ast:import-public? imp))
-          dep))
-
-  (send fd set-message-types
-        (for/list ([x (in-list (ast:root-messages root))])
-          (ast:message->descriptor% x (ast:root-package root))))
-
-  (send fd set-enum-types
-        (for/list ([x (in-list (ast:root-enums root))])
-          (ast:enum->enum-descriptor% x (ast:root-package root))))
-
-  ;; TODO: compile file options
-  ;; TODO: compile message fields
-  ;; TODO: compile enum values
-
-  (hash-set! (current-file-desc-pool)
-             (ast-source root)
-             root)
-  fd)
+;; the current scope string (e.g. "google.protobuf" or "")
+(define current-scope (make-parameter ""))
 
 
-;; convert ast to descriptor%
-;; does not compile any fields, just nested types.
-(define (ast:message->descriptor% ast name-prefix)
-  (define msgd (new descriptor%
-                    [name (ast:message-name ast)]))
-
-  (let ([nested-prefix (name-append name-prefix (ast:message-name ast))])
-    (check-in-use nested-prefix (ast-loc ast))
-
-    ;; TODO: traverse oneofs and map-fields
-    (for ([f (in-list (ast:message-fields ast))])
-      (check-in-use (name-append nested-prefix (ast:field-name f))
-                    (ast-loc f)))
-
-    (send msgd set-nested-types
-          (for/list ([x (in-list (ast:message-nested-msgs ast))])
-            (ast:message->descriptor% x nested-prefix)))
-
-    (send msgd set-nested-enums
-          (for/list ([x (in-list (ast:message-nested-enums ast))])
-            (ast:enum->enum-descriptor% x nested-prefix))))
-
-  ;; TODO: compile message options
-  msgd)
+;; reset all of the above parameters
+(define-syntax-rule (with-clean-compile body ...)
+  (parameterize ([file-descriptor-pool (make-hash)]
+                 [all-descriptors (make-hash)]
+                 [current-scope ""]
+                 [current-file-descriptor #f])
+    body ...))
 
 
-;; convert ast to enum-descriptor%
-;; does not compile values
-(define (ast:enum->enum-descriptor% ast name-prefix)
-  (define enumd (new enum-descriptor%
-                     [name (ast:enum-name ast)]))
 
-  (check-in-use (name-append name-prefix (ast:enum-name ast))
-                (ast-loc ast))
+;; convert any arbitrary ast into a descriptor
+;; this is recursive, checks for name clashes,
+;; populates all-descriptors and compiles options.
+;;
+;; but it does NOT resolve type names! that must be
+;; done in a second pass.
+;;
+;; ast->descriptor : ast? -> object%
+(define (ast->descriptor ast)
+  (match ast
+    [(struct ast:root (loc pkg imports messages enums opts)) (void)])
 
-  (send enumd set-values
-        (for/list ([ev-ast (in-list (ast:enum-values ast))]
-                   [i (in-naturals)])
-          (check-in-use (name-append name-prefix (ast:enum-val-name ev-ast))
-                        (ast-loc ev-ast))
-
-          (when (and (zero? i)
-                     (not (zero? (ast:enum-val-number ev-ast))))
-            (raise-compile-error (ast-loc ev-ast)
-                                 "first enum field must be number 0"))
-
-          ;; TODO: compile enum value options
-
-          (new enum-value%
-               [name (ast:enum-val-name ev-ast)]
-               [number (ast:enum-val-number ev-ast)])))
-
-  ;; TODO: compile enum options
-  enumd)
+  (error "unimplemented"))
