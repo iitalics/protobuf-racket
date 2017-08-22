c80930d18f3631186a415dd70d2cff23eea20479
Milo Turner <iitalics@gmail.com>
2017-08-22 14:15:14 -0400

    introducing fq-name and ur-name "types"

--- protobuf/private/compiler.rkt
+++ protobuf/private/compiler.rkt
@@ -15,21 +15,54 @@
           (list loc loc))))
 
 
-;; append a '.'-delimeted name onto another, e.g.
-;;   (name-append "a.b" "c") = "a.b.c"
-;;   (name-append "" "d.e") = "d.e"
-(define (name-append pre post)
-  (if (equal? pre "")
-      post
-      (string-append pre "." post)))
+
+
+;; fq-name = fully qualified name, e.g. ".google.protobuf.Any"
+(define (fq-name? s)
+  (and (string? s)
+       (string-prefix? s ".")))
+
+;; ur-name = unresolved name, e.g. "protobuf.Any"
+(define (ur-name? s)
+  (string? s))
+
+
+;; string? string? -> string?
+(define (qualify-in-scope name [scope (current-scope)])
+  (string-append scope "." name))
 
 ;; break scope into subscopes, e.g.
-;;   (in-subscopes "a.b.c") => "a", "a.b", "a.b.c"
+;;   (in-subscopes ".a.b.c") => ".a", ".a.b", ".a.b.c"
 ;;
 ;; in-subscopes : string? -> (sequenceof string?)
 (define (in-subscopes scope)
-  (in-sequences (for/stream ([c (in-string scope)]
-                             [i (in-naturals)]
+  (in-sequences (for/stream ([c (in-string scope 1)]
+                             [i (in-naturals 1)]
                              #:when (char=? #\. c))
                   (substring scope 0 i))
                 (in-value scope)))
+
+
+;; descriptors that are finished being compiled
+;;   (e.g. from a previous pass)
+;;
+;; fq-name? => dsctor?
+(define all-descriptors
+  (make-parameter (make-hash)))
+
+;; descriptors being compiled that need additional passes
+;; (listof (cons fq-name? dsctor?))
+(define current-unresolved-descriptors
+  (make-parameter '()))
+
+;; current scope string, e.g. "" or ".google.protobuf"
+;; (or/c "" fq-name?)
+(define current-scope
+  (make-parameter ""))
+
+;; fully-qualified name of current oneof
+;; for use in compiling fields nested in a oneof
+;; (or/c #f fq-name?)
+(define current-oneof-fq-name
+  (make-parameter #f))
+
