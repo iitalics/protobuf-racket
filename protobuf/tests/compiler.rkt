d0d60bf484c203ec2e4dcd7c87d2baed47e898d0
Milo Turner <iitalics@gmail.com>
2017-08-22 14:28:06 -0400

    test for oneofs

--- protobuf/tests/compiler.rkt
+++ protobuf/tests/compiler.rkt
@@ -36,24 +36,23 @@
 
     (check-exn (λ (e) (regexp-match? #px"name \\.test1\\.A\\.x already used"
                                      (exn-message e)))
-               (λ () (compile-root/tmp
-                      "syntax = 'proto3';"
-                      "package test1;"
-                      "message A {"
-                      "  uint32 x = 1;"
-                      "  enum x { }"
-                      "}")))
+               (λ () (compile-root/tmp "syntax = 'proto3';"
+                                       "package test1;"
+                                       "message A {"
+                                       "  uint32 x = 1;"
+                                       "  enum x { }"
+                                       "}")))
+
 
     (check-not-exn
-     (λ () (let* ([r (compile-root/tmp
-                      "syntax = 'proto3';"
-                      "package test2;"
-                      "message A {"
-                      "  uint32 x = 1;"
-                      "  enum E {"
-                      "    V = 0;"
-                      "  }"
-                      "}")]
+     (λ () (let* ([r (compile-root/tmp "syntax = 'proto3';"
+                                       "package test2;"
+                                       "message A {"
+                                       "  uint32 x = 1;"
+                                       "  enum E {"
+                                       "    V = 0;"
+                                       "  }"
+                                       "}")]
 
                   [A (hash-ref (all-descriptors) ".test2.A")]
                   [A.x (hash-ref (all-descriptors) ".test2.A.x")]
@@ -64,10 +63,40 @@
              (check-pred dsctor:field? A.x)
              (check-pred dsctor:enum? A.E)
              (check-pred dsctor:enum-value? A.V)
+             (check-false (dsctor:field-repeated? A.x))
              (check-equal? (dsctor:field-number A.x) 1)
              (check-equal? (dsctor:enum-value-number A.V) 0)
              (check-equal? (dsctor:message-nested-enums A) '(".test2.A.E"))
              (check-equal? (dsctor:enum-values A.E) '(".test2.A.V"))
+             )))
+
+
+    (check-not-exn
+     (λ () (let* ([r (compile-root/tmp "syntax = 'proto3';"
+                                       "package test3;"
+                                       "message A {"
+                                       "  repeated uint32 x = 1;"
+                                       "  oneof O {"
+                                       "    uint32 y = 2;"
+                                       "    uint32 z = 3;"
+                                       "  }"
+                                       "}")]
+
+                  [A (hash-ref (all-descriptors) ".test3.A")]
+                  [A.x (hash-ref (all-descriptors) ".test3.A.x")]
+                  [A.y (hash-ref (all-descriptors) ".test3.A.y")]
+                  [A.z (hash-ref (all-descriptors) ".test3.A.z")]
+                  [A.O (hash-ref (all-descriptors) ".test3.A.O")])
+
+             (check-pred dsctor:message? A)
+             (check-pred dsctor:field? A.x)
+             (check-pred dsctor:oneof? A.O)
+             (check-equal? (dsctor:message-fields A) '(".test3.A.x" ".test3.A.y" ".test3.A.z"))
+             (check-equal? (dsctor:field-oneof A.x) #f)
+             (check-equal? (dsctor:field-oneof A.y) ".test3.A.O")
+             (check-equal? (dsctor:field-oneof A.z) ".test3.A.O")
+             (check-true (dsctor:field-repeated? A.x))
+             (check-false (dsctor:field-repeated? A.z))
              )))
 
 
