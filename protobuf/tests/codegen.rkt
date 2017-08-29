8e155667a870f27776859706eb0e195329e67f1f
Milo Turner <iitalics@gmail.com>
2017-08-29 17:24:27 -0400

    more precise tests (specifically some identifiers)

--- protobuf/tests/codegen.rkt
+++ protobuf/tests/codegen.rkt
@@ -7,6 +7,9 @@
            "../private/compiler.rkt"
            "../private/codegen.rkt")
 
+  (define-syntax-rule (check-free-id=? arg ...)
+    (check free-identifier=? arg ...))
+
   (parameterize ([all-descriptors (make-hash)])
 
     (check-not-exn
@@ -21,24 +24,30 @@
                                 "}"
                                 #:implementations '(".test1.Color"))])
 
-         (check-pred (syntax-parser
-                       #:literals (λ define-values case else quote)
-                       #:datum-literals (Red Green Blue)
-                       [(define-values (E-> ->E E? def-E)
-                          (_ (λ (_) (case _
-                                      [(Red) '0]
-                                      [(Green) '1]
-                                      [(Blue) '2]
-                                      [else #f]))
-                             (λ (_) (case _
-                                      [(0) 'Red]
-                                      [(1) 'Green]
-                                      [(2) 'Blue]
-                                      [else #f]))
-                             (_ 'Red 'Green 'Blue)
-                             (begin0 'Red 'Green 'Blue)))
-                        #t]
-                       [_ #f])
-                     (implement (first impls))))))
+         (define impl:Color (first impls))
+
+         (syntax-parse (implement impl:Color)
+           #:literals (λ define-values case else quote)
+           #:datum-literals (Red Green Blue)
+           [(define-values (E-> ->E E? def-E)
+              (_ (λ (_) (case _
+                          [(Red) '0]
+                          [(Green) '1]
+                          [(Blue) '2]
+                          [else #f]))
+                 (λ (_) (case _
+                          [(0) 'Red]
+                          [(1) 'Green]
+                          [(2) 'Blue]
+                          [else #f]))
+                 (_ 'Red 'Green 'Blue)
+                 (begin0 'Red 'Green 'Blue)))
+
+            (check-free-id=? #'E? (implementation-pred-id impl:Color))
+            (check-free-id=? #'def-E (implementation-default-id impl:Color))]
+
+           [s
+            (fail (format "impl syntax: ~v"
+                          (syntax->datum #'s)))]))))
 
     ))
